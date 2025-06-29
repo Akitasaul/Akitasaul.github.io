d254246423a05d1c62da0e13e255cfef415c10c5
Akitasaul <zaratesaul52@gmail.com>
2025-06-29 05:14:23 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+function setup() {
+  createCanvas(400, 600);
+}
+
+function draw() {
+  background(135, 206, 235);
+  //sun
+  fill(255, 223, 0);
+  ellipse(350, 80, 80, 80);
+  noStroke();
+  //clouds
+  fill(255);
+  ellipse(80, 100, 60, 60);
+  ellipse(100, 90, 60, 70);
+  ellipse(120, 100, 60 , 60);
+   
+  //rectangle
+  rect(200, 300, 5, 40);
+  rect(165, 300, 5, 40);
+  fill(300,14,180);
+  rect(157, 320, 55, 40);
+//ellipse
+  fill(28,28,228);
+  ellipse(182, 230, 160);
+  fill(144,29,60);
+  ellipse(182, 260, 60);
+  fill(123,76,76);
+  ellipse(182, 180, 60);
+  fill(23,76,76);
+  ellipse(230, 219, 60);
+   fill(245,121,19);
+  ellipse(133, 219, 60);
+  
+  
+  //rectangle
+  fill(229,198,50);
+  rect(157, 505, 95);
+  fill(165,15,20);
+  rect(168, 505, 15);
+  rect(197, 505, 15);
+  rect(225, 505, 15);
+  rect(225, 535, 15);
+  rect(197, 535, 15);
+  rect(168, 535, 15);
+  rect(188, 570, 30);
+  
+  
+  
+  
+  
+  }
+
+
+
